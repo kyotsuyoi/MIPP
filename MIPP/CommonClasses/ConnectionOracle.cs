5d99bfc9a15b1633f90afa9e84517882a7974bad
kyotsuyoi <kyotsuyoi@hotmail.com>
2018-03-02 09:42:48 -0300

    Update ConnectionOracle.cs

--- MIPP/CommonClasses/ConnectionOracle.cs
+++ MIPP/CommonClasses/ConnectionOracle.cs
@@ -8,7 +8,7 @@ namespace MIPP.CommonClasses
         public OracleConnection Connect = new OracleConnection("Data Source=(DESCRIPTION="
         + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.0.191)(PORT=1521)))"
         + "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));"
-        + "User Id=consinco;Password=c5_4854;");
+        + "User Id=consinco;Password=;");
         public OracleCommand cmd = new OracleCommand();
     }
 
